7e7e2b5fbbb66588d30feef0f55fa692476e8cbb
Clinic-Directory-2021 <89154737+Clinic-Directory-2021@users.noreply.github.com>
2022-05-17 00:45:32 +0800

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -82,7 +82,7 @@ export default function Login() {
             <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
               Welcome to Incident Report Portal
             </Typography>
-            <img src="/static/illustrations/illustration_login.png" alt="login" />
+            
           </SectionStyle>
         )}
 
